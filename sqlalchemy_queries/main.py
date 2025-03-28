dc389b024975f1e0ff0a8ed2ddd0d02bb80e5a34
Dmytro Kopan <koapndima@gmail.com>
2025-03-28 16:53:50 +0100

    :adhesive_bandage: Late addition of tasks

--- sqlalchemy_queries/main.py
+++ sqlalchemy_queries/main.py
@@ -1,5 +1,5 @@
 from typing import List
-from sqlalchemy import create_engine, Integer, String, Boolean, Numeric, ForeignKey
+from sqlalchemy import create_engine, Integer, String, Boolean, Numeric, ForeignKey, func, select, update
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship, sessionmaker
 # Task 1: Create an instance of the engine to connect to the in-memory SQLite database.
 # Task 2: Create a session to interact with the database using the engine you just created.
@@ -16,11 +16,11 @@ from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
 
 URL = "sqlite:///:memory:"
 sql_engine = create_engine(url=URL,
-                           echo_pool=True,
-                           echo=True)
+                              echo_pool=True,
+                              echo=True
+                           )
 
 Base = declarative_base()
-
 
 class Product(Base):
     __tablename__ = "product"
@@ -33,8 +33,7 @@ class Product(Base):
     category_id: Mapped[int] = mapped_column(
         Integer, ForeignKey('category.category_id'))
     category: Mapped['Category'] = relationship(
-        "Category", back_populates="product")
-
+        "Category", back_populates="products")
 
 class Category(Base):
     __tablename__ = "category"
@@ -43,30 +42,79 @@ class Category(Base):
     name: Mapped[str] = mapped_column(String(100), nullable=True)
     description: Mapped[str] = mapped_column(String(250))
 
-    product: Mapped['Product'] = relationship(
+    products: Mapped[list['Product']] = relationship(
         "Product", back_populates="category")
 
 
-Base.metadata.create_all(bind=sql_engine)
+def task1(session):
+    categories: List[Category] = []
+    electronic_category = Category(
+        name="Электроника", description="Гаджеты и устройства.")
+    book_category = Category(
+        name="Книги", description="Печатные книги и электронные книги.")
+    cloth_category = Category(
+        name="Одежда", description="Одежда для мужчин и женщин.")
+    categories.append(electronic_category)
+    categories.append(book_category)
+    categories.append(cloth_category)
+    products: List[Product] = []
+    products.append(Product(name="Смартфон", price=299.99,
+                    in_stock=True, category=electronic_category))
+    products.append(Product(name="Ноутбук", price=499.99,
+                    in_stock=True, category=electronic_category))
+    products.append(Product(name="Научно-фантастический роман",
+                    price=15.99, in_stock=True, category=book_category))
+    products.append(Product(name="Джинсы", price=40.50,
+                    in_stock=True, category=cloth_category))
+    products.append(Product(name="Футболка", price=20,
+                    in_stock=True, category=cloth_category))
+    session.add_all(categories)
+    session.add_all(products)
+    session.commit()
 
-SessionFabric = sessionmaker(bind=sql_engine)
-session = SessionFabric()
 
-categories: List[Category] = []
-electronic_category = Category(name="Электроника", description="Гаджеты и устройства.")
-book_category = Category(name="Книги", description="Печатные книги и электронные книги.")
-cloth_category = Category(name="Одежда", description="Одежда для мужчин и женщин.")
-categories.append(electronic_category)
-categories.append(book_category)
-categories.append(cloth_category)
-products: List[Product] = []
-products.append(Product(name="Смартфон", price=299.99, in_stock=True, category=electronic_category))
-products.append(Product(name="Ноутбук", price=499.99, in_stock=True, category=electronic_category))
-products.append(Product(name="Научно-фантастический роман", price=15.99, in_stock=True, category=book_category))
-products.append(Product(name="Джинсы", price=40.50, in_stock=True, category=cloth_category))
-products.append(Product(name="Футболка", price=20, in_stock=True, category=cloth_category))
-session.add_all(categories)
-session.add_all(products)
-session.commit()
+def task2(session):
+    for cat in session.query(Category).all():
+        print('# # '*20)
+        [print(f" Category: {cat.name}; Product name:{p.name}, price: {p.price}")
+         for p in cat.products]
 
-session.close()
+
+def task3(session):
+    stmt = update(Product).where(Product.name ==
+                                 "Смартфон").values(price=349.99)
+    session.execute(stmt)
+
+
+def task4(session):
+    result = (session
+              .query(Category.name, func.count(Product.product_id).label("product_count"))
+              .outerjoin(Category.products)
+              .group_by(Category.category_id)
+              .all())
+    for category_name, product_count in result:
+        print(f"Category: {category_name}, Product Count: {product_count}")
+
+
+def task5(session):
+    result = (session
+              .query(Category.name)
+              .outerjoin(Category.products)
+              .group_by(Category.category_id)
+              .having(func.count(Product.product_id) > 1)
+              .all())
+    [print(c) for c in result]
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=sql_engine)
+    SessionFabric = sessionmaker(bind=sql_engine)
+    session = SessionFabric()
+    task1(session=session)
+    task2(session=session)
+    task3(session=session)
+    task3(session=session)
+    task4(session=session)
+    task5(session=session)
+
+    session.close()
